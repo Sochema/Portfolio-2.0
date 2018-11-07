983f9c41a51b5a4422efd8c3cbebc5444260d90f
Sonia Chemaa <sonia.chemaa@outlook.com>
2018-11-07 23:32:39 +0100

    some test and full screen header + resize of navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+
+/* TO COLLAPSE MOBILE NAVBAR AFTER CLICKING LINK
+
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
+*/
