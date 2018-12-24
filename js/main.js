7541d04d7de0a2ce9da062d1c6e0640732995197
Sonia Chemaa <sonia.chemaa@outlook.com>
2018-12-24 21:41:50 +0100

    I don't remember what I changed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 
 
-/* TO COLLAPSE MOBILE NAVBAR AFTER CLICKING LINK
 
+/*
 $(document).ready(function () {
-  $(".navbar-nav li a").click(function(event) {
-    $(".navbar-collapse").collapse('hide');
-  });
-});
-*/
+      $(".navbar-nav li.trigger-collapse a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+      });
+    });
